19ff63ca20e07e04b119a6fce58b88677346c5ed
Mark Koyanagi <blindzombie@gmail.com>
2017-03-29 09:10:03 -0400

    rna_transcription iteration

--- exercism/ruby/rna-transcription/rna_transcription.rb
+++ exercism/ruby/rna-transcription/rna_transcription.rb
@@ -1,16 +1,14 @@
 class Complement
-  def self.of_dna(dna_strand)
-    dna = dna_strand.split('')
-    mapping = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
-    rna_strand = ''
-    dna.each do |nucleotide|
-      unless mapping.has_key?(nucleotide)
-        rna_strand = ''
-        break
-      end
-      rna_nucleotide = mapping.fetch(nucleotide)
-      rna_strand << rna_nucleotide
+  MAPPING = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
+  def self.of_dna(dna)
+    begin
+      rna = dna.chars.map{ |c| MAPPING.fetch(c) }.join
+    rescue KeyError
+      return ''
     end
-    return rna_strand
   end
+end
+
+module BookKeeping
+  VERSION = 4
 end
